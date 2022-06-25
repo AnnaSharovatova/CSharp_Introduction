017b2e6e8a2e15e0a93719a53c24500cab875d55
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-25 19:13:13 +0300

    Examples 008 FriendsAndDog project

---
+++ Examples/Examples008_FriendsAndDog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distance = 10000;
+int FFS = 1;
+int SFS = 2;
+int DS = 5;
+int Friend = 2;
+int time = 0;
+
+while(distance > 10)
+{
+    if(Friend == 1)
+    {
+        time = distance / (FFS + DS);
+        Friend = 2;
+    }
+    else
+    {
+        time = distance / (SFS + DS);
+        Friend = 1;
+    }
+    distance = distance - (FFS + SFS) * time;
+    count += 1;
+}
+Console.WriteLine("Собака бежит "); 
+Console.WriteLine(count); 
+Console.WriteLine("раз"); 
