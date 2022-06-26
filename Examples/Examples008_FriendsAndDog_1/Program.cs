07f41fbe847c0e87795764e44cac5d0fa1e1897d
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-26 17:24:46 +0300

    Альтернативный метод решения задачи с вводом данных от пользователя и  конвертацией переменной в число с плавающей запятой

---
+++ Examples/Examples008_FriendsAndDog_1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("Введите скорость первого: ");
+var FirstFriendSpeed = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите скорость второго: ");
+var SecondFriendSpeed = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите скорость собаки: ");
+var DogSpeed = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите расстояние между друзьями: ");
+var distance = Convert.ToSingle(Console.ReadLine());
+
+int distanceLimit = 10;
+bool DogDirection = true;
+int DogCounter = 0;
+Single timeToMeet = 0;
+
+while (distance > distanceLimit)
+{
+    if (DogDirection == true)
+    {
+        timeToMeet = distance / (FirstFriendSpeed + DogSpeed);
+        DogDirection = false;
+    }
+    else
+    {
+        timeToMeet = distance / (SecondFriendSpeed + DogSpeed);
+        DogDirection = true;  
+    }
+    distance = distance - timeToMeet * (FirstFriendSpeed + SecondFriendSpeed);
+    DogCounter ++;
+    Console.WriteLine(distance);
+}
+Console.WriteLine("Собака пробежала " + DogCounter + " раз (а)");
